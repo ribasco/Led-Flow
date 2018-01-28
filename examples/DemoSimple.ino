6f559b2fefe7f3b5bd8c52ac7fe6cfbb6c904c33
Rafael Ibasco <raffy@ibasco.com>
2018-01-28 14:44:12 +0800

    Added ledSingle example under DemoSimple
    Change-Id: I48bfc5b424c73373c7308a82ce8cc0addb1121e2

--- examples/DemoSimple.ino
+++ examples/DemoSimple.ino
@@ -5,20 +5,32 @@ using namespace LedFlow;
 #define PIN_LED_STATUS_R  2
 #define PIN_LED_STATUS_G  3
 #define PIN_LED_STATUS_B  4
+#define PIN_LED_SINGLE    5
+
+/**
+ * This is a demonstration on how to manually 
+ */
 
 LedRgb ledRgb = LedRgb(PIN_LED_STATUS_R, PIN_LED_STATUS_G, PIN_LED_STATUS_B);
+LedSingle ledSingle = LedSingle(PIN_LED_SINGLE);
 
 void setup() {
 
 }
 
-uint8_t red = 0;
+uint8_t value = 0;
 
 void loop() {
-  // Immediately write the value (using the macro RGB(red, green, blue)) to the gpio pin.
-  // As you can see, there is no need to call the update()
-  // method when calling setValue().
-  if (red > 255)
-    red = 0;
-  ledRgb.setValue(RGB(red++, 0, 0));
+	// Immediately write the value (using the macro RGB(red, green, blue)) to the gpio pin.
+	// Note: Calling the update() method of the led instance is not necessary when using the setValue() method.
+	if (red > 255)
+		red = 0;
+	ledRgb.setValue(RGB(value++, 0, 0));
+	
+	//Note: If you need to individually set the color without having to override the values of the other leds, do this: 
+	//ledRgb.setValueRed(redValue);
+	//ledRgb.setValueGreen(greenValue);
+	//ledRgb.setValueBlue(blueValue);
+	
+	ledSingle.setValue(value);
 }
