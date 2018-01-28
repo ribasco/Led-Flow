d24e67d1253e20059fcc2c9bcfb1c1a69ec4ce4e
Rafael Ibasco <raffy@ibasco.com>
2018-01-28 12:16:22 +0800

    Added demo simple example

---
+++ examples/DemoSimple.ino
@@ -0,0 +1,24 @@
+#include <Led.h>
+
+using namespace LedLib;
+
+#define PIN_LED_STATUS_R  2
+#define PIN_LED_STATUS_G  3
+#define PIN_LED_STATUS_B  4
+
+LedRgb ledRgb = LedRgb(PIN_LED_STATUS_R, PIN_LED_STATUS_G, PIN_LED_STATUS_B);
+
+void setup() {
+	
+}
+
+uint8_t red = 0;
+
+void loop() {
+	// Immediately write the value (using the macro RGB(red, green, blue)) to the gpio pin.
+	// As you can see, there is no need to call the update() 
+	// method when calling setValue().
+	if (red > 255)
+		red = 0;
+	ledRgb.setValue(RGB(red++, 0, 0));
+}
